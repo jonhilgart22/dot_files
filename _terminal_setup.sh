d52cc5c1f2bcf83a2d9f5ad50ea55d5042cafa90
Jonathan Hilgart <jonhilgart22@users.noreply.github.com>
2019-02-15 08:39:27 -0800

    Update _terminal_setup.sh

--- _terminal_setup.sh
+++ _terminal_setup.sh
@@ -43,6 +43,23 @@ function install_powerlevel9k {
   echo "Don't forget to update the font in iTerm to 'Hack Regular Nerd Font Complete'"
 }
 
+#### Git-so-fancy colors
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "yellow"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+
+#####
 install_oh_my_zsh
 switch_to_zsh
 install_powerlevel9k
